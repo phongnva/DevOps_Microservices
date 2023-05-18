8d631a7cbfff39113010c12288585470292cad89
Nguyen Vu Anh Phong (IT.DN) <PhongNVA@fpt.com>
2023-05-18 23:54:30 +0700

    First commit

--- app.py
+++ app.py
@@ -60,9 +60,11 @@ def predict():
     LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
     # scale the input
     scaled_payload = scale(inference_payload)
+    LOG.info(f"the scaled input: {scaled_payload}")
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"The resultant prediction: {prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
